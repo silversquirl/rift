9abee1f2a10a211eb8a5b8d0bd7bb2ca6f1b8ee5
Samadi van Koten <samadi@vktec.org.uk>
2020-12-05 20:08:33 +0000

    Add configuration file support

---
+++ games/portal2/config.tcl
@@ -0,0 +1,86 @@
+title "Portal 2" "Single Player (No SLA)"
+timer
+splits {
+	"Chapter 1" {
+		"Container Ride" .
+		"Portal Carousel" .
+		"Portal Gun" .
+		"Smooth Jazz" .
+		"Cube Momentum" .
+		"Future Starter" .
+		"Secret Panel" .
+		"Wakeup" .
+		"Incinerator" .
+	}
+	"Chapter 2" {
+		"Laser Intro" .
+		"Laser Stairs" .
+		"Dual Lasers" .
+		"Laser Over Goo" .
+		"Catapult Intro" .
+		"Trust Fling" .
+		"Pit Flings" .
+		"Fizzler Intro" .
+	}
+	"Chapter 3" {
+		"Ceiling Catapult" .
+		"Ricochet" .
+		"Bridge Intro" .
+		"Bridge the Gap" .
+		"Turret Intro" .
+		"Laser Relays" .
+		"Turret Blocker" .
+		"Laser vs. Turret" .
+		"Pull the Rug" .
+	}
+	"Chapter 4" {
+		"Column Blocker" .
+		"Laser Chaining" .
+		"Triple Laser" .
+		"Jailbreak" .
+		"Escape" .
+	}
+	"Chapter 5" {
+		"Turret Factory" .
+		"Turret Sabotage" .
+		"Neurotoxin Sabotage" .
+		"Tube Ride" .
+		"Core" .
+	}
+	"Chapter 6" {
+		"Long Fall" .
+		"Underground" .
+		"Cave Johnson" .
+		"Repulsion Intro" .
+		"Bomb Flings" .
+		"Crazy Box" .
+		"PotatOS" .
+	}
+	"Chapter 7" {
+		"Prop Intro" .
+		"Prop Flings" .
+		"Conversion Intro" .
+		"Three Gels" .
+	}
+	"Chapter 8" {
+		"Test" .
+		"Funnel Intro" .
+		"Ceiling Button" .
+		"Wall Button" .
+		"Polarity" .
+		"Funnel Catch" .
+		"Stop the Box" .
+		"Laser Catapult" .
+		"Laser Platform" .
+		"Prop Catch" .
+		"Repulsion Polarity" .
+	}
+	"Chapter 9" {
+		"Finale 1" .
+		"Finale 2" .
+		"Finale 3" .
+		"Finale 4" .
+	}
+}
+
+splitter "| ./splitters/sar/sar_split"
